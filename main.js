02174e68cfc0869a2c12ad042ebdf0d5ad87ce09
Joaquin Munoz <jmadpei@gmail.com>
2022-08-26 11:28:38 -0400

    fucion del menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
-const destopMenu = document.querySelector('.desktop-menu');
+const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 
 menuEmail.addEventListener('click', toggleDestopMenu)
+menuHamIcon.addEventListener('click', toggleMobileMenu)
 
 function toggleDestopMenu() {
-    destopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
