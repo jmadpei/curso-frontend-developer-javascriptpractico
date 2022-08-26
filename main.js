a5fccd3a8b43e3ac8a4454beefcbe9eae71ace93
Joaquin Munoz <jmadpei@gmail.com>
2022-08-26 12:48:27 -0400

    Carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
-
-menuEmail.addEventListener('click', toggleDestopMenu)
-menuHamIcon.addEventListener('click', toggleMobileMenu)
+menuEmail.addEventListener('click', toggleDestopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
+menuCarritoIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDestopMenu() {
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if (!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
+
+
     mobileMenu.classList.toggle('inactive');
+
+
 }
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+ 
+    if (!isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+}
+
